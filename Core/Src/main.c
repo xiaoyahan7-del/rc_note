d48d5fd775fa8713295e01605b932caf23cab0d2
Xiaoyahan <xiaoyahan7@gmail.com>
2026-08-07 19:12:05 +0800

    start from led2

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -51,7 +51,7 @@
 #define LED_COUNT   4U    /* 板载 LED 数量，U 表示 unsigned 无符号数 */
 #define BLINK_TIMES 3U    /* 每颗 LED 闪烁次数 */
 #define DELAY_MS    500U  /* LED 亮/灭持续时间，单位毫秒 */
-#define BEEP_MS     120U  /* 蜂鸣器响一声的时长 */
+#define BEEP_MS     300U  /* 蜂鸣器响一声的时长 */
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -127,7 +127,7 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-    current_led = 1U; /* 练习点：改成 2U，观察从哪颗 LED 开始 */
+    current_led = 2U; /* 练习点：改成 2U，观察从哪颗 LED 开始 */
 
     /* while 循环：条件成立就反复执行 {} 里的代码 */
     while (current_led <= led_count)
@@ -141,7 +141,7 @@ int main(void)
     /* if / else 判断：让延时每次变快一点，到 100 后重新回到初始值 */
     if (delay_ms > 100U)
     {
-      delay_ms -= 20U; /* 练习点：改成 += 20U 看速度变化方向 */
+      delay_ms += 20U; /* 练习点：改成 += 20U 看速度变化方向 */
     }
     else
     {
