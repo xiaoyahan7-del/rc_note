0f0dd0a6aba88a1c9e729eca3dfdf1126cee4987
Xiaoyahan <xiaoyahan7@gmail.com>
2026-08-13 21:22:27 +0800

    实现按键状态切换、流水灯和呼吸灯

--- user/inc/led.h
+++ user/inc/led.h
@@ -4,12 +4,13 @@
 #include "main.h"
 
 
-#define LED1_PIN GPIO_PIN_5
-#define LED2_PIN GPIO_PIN_7
-#define LED3_PIN GPIO_PIN_4
-#define LED4_PIN GPIO_PIN_6
+#define LED1_PIN GPIO_PIN_4
+#define LED2_PIN GPIO_PIN_5
+#define LED3_PIN GPIO_PIN_6
+#define LED4_PIN GPIO_PIN_7
 
 #define LED_PORT GPIOA
+
 
 
 void led_on(uint16_t pin);
